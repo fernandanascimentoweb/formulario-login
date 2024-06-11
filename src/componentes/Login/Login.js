ca140e8236b98e8aedfc2e4c7d72dafca42cf609
fernandanascimentoweb <fernanda.nascimentoweb@gmail.com>
2024-06-11 20:29:51 -0300

    first commit

---
+++ src/componentes/Login/Login.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {FaUser, FaLock} from "react-icons/fa";
+import { useState } from "react";
+import './Login.css';
+
+    
+    const Login = () => {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        alert("Enviando os dados:" + username + " - " + password);
+
+        console.log("teste", username, password);
+        console.log("envio");
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Acesse o sitema</h1>
+                <div className="input-field">
+                    <input type="email" placeholder="E-mail"
+                    onChange={(e)=> setUsername(e.target.value)} /> 
+                    <FaUser className="icon" />
+                </div>
+                <div className="input-field">
+                    <input type="password" placeholder="Senha"
+                    onChange={(e)=> setPassword(e.target.value)} />
+                    <FaLock className="icon" />
+                </div>
+
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox" />
+                        Lembre de mim 
+                    </label>
+                    <a href="#" > Esqueceu a senha ?</a>
+                </div>
+
+                <button>Entrar</button>
+
+                <div className="signup-link">
+                    <p>
+                    Não tem conta? <a href="#">Registrar</a>
+                    </p>
+                </div>
+
+            </form>
+
+        </div>
+
+        );
+    };
+
+    export default Login;
